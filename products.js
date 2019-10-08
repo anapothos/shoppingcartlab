c4d8f176384462b5cbc245372dec5a8fa44e1db4
anapothos <anapothos@gmail.com>
2019-10-08 09:02:32 -0700

    added product info to initial files

---
+++ products.js
@@ -0,0 +1,33 @@
+const fingernails = {
+    id: 'fingernails',
+    name: 'Finger Nails',
+    image: 'fingernails.jpg',
+    description: 'Carefully removed for your satisfaction.',
+    category: 'remains',
+    price: 99.99,
+    cost: 0.00,
+};
+const starbucksCup = {
+    id: 'starbuckscup',
+    name: 'Empty Starbucks Cup',
+    image: 'starbucks.jpg',
+    description: 'Procured from the streets and inspected to ensure optimal filth.',
+    category: 'remains',
+    price: 79.99,
+    cost: 0.00,
+};
+const lostShoe = {
+    id: 'lostshoe',
+    name: 'Abandoned Shoe',
+    image: 'shoe.jpg',
+    description: 'Discovered on the South Bank, lost and alone.',
+    category: 'remains',
+    price: 79.99,
+    cost: 0.00,
+};
+const waste = [
+    lostShoe,
+    starbucksCup,
+    fingernails,
+];
+export default waste; 
